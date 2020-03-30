f257a1784926aa8e2bb14a74dd9aba7c9cdcf14b
Timvdperk <timvdperk@gmail.com>
2020-03-30 11:56:16 +0200

    Router progress, not working yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function Dashboard() {
+    return document.getElementById("/public/Dashboard.html");
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
+
